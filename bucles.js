9d1591a2cf21791682e5fe65dd8eab564090db0c
FedeCasper <federicorouyere@gmail.com>
2023-03-29 19:07:24 -0300

    Ejercicio 3 bucles

--- bucles.js
+++ bucles.js
@@ -15,15 +15,34 @@ console.log("Bucles");
 
 // do{
      
-//      num = prompt("Ingrese un número para ver su tabla")
+//      num = prompt( "Ingrese para sumar (interrumpa con vacio)" )
 //      if(num !== ""){
 //           console.log(total = parseInt(total) + parseInt(num)) 
+//           if(total > 99){
+//                console.log("El juego solo llega hasta 99");
+//                break
+//           }
 //      }else{
 //           console.log("Fin") 
 //      }
 // }while(num != "")
 
+// console.log(total);
 // console.log("se cortó");
 
-//Ejercicio N°
+//Ejercicio N° 3
 // ----------------------------------------------------------------------------
+// let numeroIngresado = ""
+// numeroIngresado = prompt(`Ingrese un número`)
+
+//      while(total != numeroIngresado){
+//           if(numeroIngresado < total){
+//                numeroIngresado = prompt(`El número oculto es mayor a ${numeroIngresado} ❌`)
+//           }else if(numeroIngresado > total){
+//                numeroIngresado = prompt(`El número oculto es menor a ${numeroIngresado} ❌`)
+//           }
+               
+//      }
+// alert("adivinaste ✅")
+
+
