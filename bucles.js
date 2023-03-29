5bc73289a0400633ed740dd32a2fbe1f7e2a6b3a
FedeCasper <federicorouyere@gmail.com>
2023-03-29 18:09:36 -0300

    Ejercicios Bucles 3

---
+++ bucles.js
@@ -0,0 +1,29 @@
+console.log("Bucles");
+
+//Ejercicio N°1
+// ----------------------------------------------------------------------------
+// let num = prompt("Ingrese un número para ver su tabla")
+
+// for(let i = 1 ; i < 11 ; i++){
+//      console.log(`${i} x ${num} = ${i*num}`);
+// }
+
+//Ejercicio N°2
+// ----------------------------------------------------------------------------
+// let num = ""
+// let total = 0
+
+// do{
+     
+//      num = prompt("Ingrese un número para ver su tabla")
+//      if(num !== ""){
+//           console.log(total = parseInt(total) + parseInt(num)) 
+//      }else{
+//           console.log("Fin") 
+//      }
+// }while(num != "")
+
+// console.log("se cortó");
+
+//Ejercicio N°
+// ----------------------------------------------------------------------------
