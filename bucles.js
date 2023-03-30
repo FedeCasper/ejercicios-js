62e3db228788db154c895a23f79d0a88a8d5f267
FedeCasper <federicorouyere@gmail.com>
2023-03-30 12:27:47 -0300

    Ejercicio 5 bucles

--- bucles.js
+++ bucles.js
@@ -45,4 +45,40 @@ console.log("Bucles");
 //      }
 // alert("adivinaste ✅")
 
+//Ejercicio N° 4
+// ----------------------------------------------------------------------------
+
+//Solución 1
+//let num = prompt("Ingrese un número")
+
+// while((num%2) == 0 || (num%3) == 0 || (num%5) == 0 || (num%7) == 0){
+     
+//      console.log(`${(num%2)} es el resto dividido 2`);
+//      console.log(`${(num%3)} es el resto dividido 3`);
+//      console.log(`${(num%5)} es el resto dividido 5`);
+//      console.log(`${(num%7)} es el resto dividido 7`);
+//      console.log((num%2) == 0 || (num%3) == 0 || (num%5) == 0 || (num%7) == 0);
+//      num = prompt("el numero no es primo ingrese otro")
+// }
+// console.log(`${(num%2)} es el resto dividido 2`);
+// console.log(`${(num%3)} es el resto dividido 3`);
+// console.log(`${(num%5)} es el resto dividido 5`);
+// console.log(`${(num%7)} es el resto dividido 7`);
+// console.log((num%2) == 0 || (num%3) == 0 || (num%5) == 0 || (num%7) == 0);
+// alert(`El número ${num} es primo!`)
+
+//Solución 2
+//let num = prompt("Ingrese un número")
+
+// for(let i = 2 ; i < num ; i++){
+//      console.log(num%i);
+//      if(num%i == 0){
+//           console.log(`${num} % ${i} == ${num%i}`);
+//           console.log("No es primo");
+//           break
+//      }else{
+//           console.log(`${num} % ${i} == ${num%i}`);
+//      }
+// }
+// alert("Es número primo")
 
